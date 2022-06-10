fb1eebf0f7486c500f6baa1c69f2887fcb09fbff
dlford <dan-github2@dlford.co>
2022-06-09 23:16:40 -0700

    mock working

--- src/components/04-Users/Users.spec.js
+++ src/components/04-Users/Users.spec.js
@@ -1,34 +1,55 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import Users from "./Users";
 
 // Create some mock data
 const mockUsers = [
   {
     id: 1,
-    username: "user1",
+    username: "potato",
   },
   {
-    id: 2,
-    username: "user2",
+    id: 7,
+    username: 42,
+  },
+  {
+    id: 3,
+    username: new Date(Date.now()).toLocaleString(),
   },
 ];
 
-// Mock the hook file
+// Since the 'getUsers' function is async, we need to mock the
+// response separately, we'll use it down below to pause the
+// test until execution has finished
+const mockPromise = Promise.resolve(mockUsers);
+
+// Mock the hook file, the default function is 'useApi', which
+// returns an object containing 'getUsers', which should return
+// our moce response from above
 jest.mock("./useApi", () => ({
   __esModule: true,
   default: () => ({
-    getUsers: () => Promise.resolve(mockUsers),
+    getUsers: () => mockPromise,
   }),
 }));
 
 describe("Users Component", () => {
-  it("Renders a list of users", () => {
-    const { getByText } = render(
-      <Users /> 
-    );
-    expect(getByText(/user1/i)).toBeInTheDocument();
-    expect(getByText(/user2/i)).toBeInTheDocument();
+  it("Renders a list of users", async () => {
+    // This time we're importing 'screen' from the testing library
+    // as well, it works the same as before, just a different syntax
+    render(<Users />);
+
+    // Wait for the mocked response to resolve before checking the
+    // assertions, otherwise we would not see the data rendered yet
+    await act(async () => {
+      await mockPromise;
+    })
+
+    // Assert that all mocked users' usernames are in the document
+    mockUsers.forEach(({ username }) => {
+      const expr = new RegExp(username, "i");
+      expect(screen.getByText(expr)).toBeInTheDocument();
+    })
   });
 });
